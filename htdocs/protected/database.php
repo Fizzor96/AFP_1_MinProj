06ec0e38692d30209e3f2bee532824812612280e
Berkes Zsolt <zsoltiberkes2@gmail.com>
2020-10-06 11:36:46 +0200

    Update: database manager: 3 függvény hozzáadva

--- htdocs/protected/database.php
+++ htdocs/protected/database.php
@@ -12,6 +12,34 @@ function getRecord($queryString, $queryParams = []) {
 	$result = $success ? $statement->fetch(PDO::FETCH_ASSOC) : [];
 	$statement->closeCursor();
 	$connection = null;
+    return $result;
+    
+function getList($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$result = $success ? $statement->fetchAll(PDO::FETCH_ASSOC) : [];
+	$statement->closeCursor();
+	$connection = null;
+	return $result;
+}
+
+function executeDML($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$statement->closeCursor();
+	$connection = null;
+	return $success;
+}
+
+function getField($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$result = $success ? $statement->fetch()[0]: [];
+	$statement->closeCursor();
+	$connection = null;
 	return $result;
 }
 ?>
