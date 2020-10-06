2cb1328204d224410ea0e182a481b8cb29c37fd6
Berkes Zsolt <zsoltiberkes2@gmail.com>
2020-10-06 21:51:29 +0200

    Update: konyvlista.php Táblázat kialakitása

---
+++ htdocs/protected/normal/konyvlista.php
@@ -0,0 +1,12 @@
+<table class="table table-striped table-blue lista" style="padding-top: 25px;">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Cím</th>
+      <th scope="col">Műfaj</th>
+      <th scope="col">Törlés</th>
+      <th scope="col">Módosítás</th>
+      <th scope="col"><a href="index.php?P=addkonyv">Könyv felvétele</th>
+    </tr>
+  </thead>
+</table>
