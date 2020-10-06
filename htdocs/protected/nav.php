7947ad1fb018b6e1a1d27498b307498bc64b4c3e
Alexandra Jambor <ja20000907@gmail.com>
2020-10-06 15:27:40 +0200

    nav.php update
    Főoldal menüpont hozzáadása a nav.php-hoz

---
+++ htdocs/protected/nav.php
@@ -0,0 +1,8 @@
+<?php
+require_once PROTECTED_DIR.'user/login.php'; 
+?>
+<ul class="nav justify-content-center" style="border: 1px solid #3b6978; width:1000px; margin:0 auto; background-color: #3b6978; margin-bottom:50px; ">
+  <li class="nav-item">
+  <a class="nav-link" href="index.php?P=home" style="color:white">Főoldal</a>
+  </li>
+  
