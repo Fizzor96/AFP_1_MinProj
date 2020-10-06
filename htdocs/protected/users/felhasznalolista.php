cd4095864c18fc567f563cfe4d1ea69765e5182b
Berkes Zsolt <zsoltiberkes2@gmail.com>
2020-10-06 21:33:37 +0200

    Update: felhasznalolista.php Táblázat kialakítása

---
+++ htdocs/protected/users/felhasznalolista.php
@@ -0,0 +1,10 @@
+<table class="table table-striped table-blue lista" style="padding-top: 25px;">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Felhasználónév</th>
+      <th scope="col">Email</th>
+      <th scope="col">Jogkör</th>
+    </tr>
+  </thead>
+</table>
