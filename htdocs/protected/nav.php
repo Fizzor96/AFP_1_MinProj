273de7fc34c8b8bb3a05a562018476691cf9c7c0
Berkes Zsolt <zsoltiberkes2@gmail.com>
2020-10-07 19:15:43 +0200

    Update: nav.php hiba javítva

--- htdocs/protected/nav.php
+++ htdocs/protected/nav.php
@@ -1,5 +1,5 @@
 <?php
-require_once PROTECTED_DIR.'user/login.php'; 
+require_once PROTECTED_DIR.'users/login.php'; 
 ?>
 <ul class="nav justify-content-center" style="border: 1px solid #3b6978; width:1000px; margin:0 auto; background-color: #3b6978; margin-bottom:50px; ">
   <li class="nav-item">
